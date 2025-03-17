2527512c1088afd09015581196b22300b76ee255
ZazaDev <everaldoallan@gmail.com>
2025-03-17 15:58:49 -0300

    Colored nodes and Dijkstra

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
+Coisas feitas até agora:
+- Criação e leitura do arquivo .ini contendo as configurações das localizações e pokémons;
+- Representação dos caminhos utilizando grafos (biblioteca networkx);
+- Começo e fim coloridos em verde e vermelho respectivamente;
+- Cálculo do caminho mais curto utilizando Dijkstra.
+
+Coisas para fazer:
+- Movimentação pelo terminal (listar os vizinhos e escolher um lugar);
+- Atualização dinâmica do mapa ao movimentar-se;
+- Sistema de stamina (sei lá, não entendi a proposta da win condition).
+
 O jogo se desenvolverá da seguinte forma:
 - Utilizando grafos, mapeamos uma "cidade" para o jogador se locomover, com diferentes distâncias.
 - O jogador poderá planejar essas rotas, tendo como condição de vitória a passagem por todos os locais de forma otimizada.
--- main.py
+++ main.py
@@ -2,6 +2,17 @@ import configparser
 import networkx as nx
 import matplotlib.pyplot as plt
 
+# TODO: pode ser otimizado só atualizando o nó viajado e o src
+def updateNodes(G, source, dest):
+  plt.close('all')
+  color_map = ['green' if node == source else 'red' if node == dest else 'lightblue' for node in G] 
+  nx.draw_networkx_nodes(G, pos, node_size=500, node_color=color_map, node_shape='8')
+  ax = plt.gca()
+  ax.margins(0.08)
+  plt.axis("off")
+  plt.tight_layout()
+  plt.show()
+
 Config = configparser.ConfigParser()
 Config.read("config.ini")
 
@@ -13,7 +24,15 @@ locs = Config.sections()[2:]
 
 for location in locs:
   for key in Config[location]:
-    G.add_edge(location, key, weight=Config[location][key])
+    G.add_edge(location, key, weight=int(Config[location][key]))
+
+
+# Calculates the shortest distance between spawnpoint and the objective
+source = Config['Locations']['Spawnpoint']
+dest = Config['Locations']['FinalBattle']
+
+print(f"\n\nShortest path: {nx.dijkstra_path(G, source, dest)}")
+print(f"Length: {nx.dijkstra_path_length(G, source, dest)}")
 
 
 # Print graph information
@@ -25,17 +44,24 @@ print("Edges:", G.edges)
 pos = nx.spring_layout(G, weight="weight", seed=7)
 
 # Nodes and edges
-nx.draw_networkx_nodes(G, pos, node_size=500, node_color='lightblue', node_shape='8')
+color_map = ['green' if node == source else 'red' if node == dest else 'lightblue' for node in G] 
+
+nx.draw_networkx_nodes(G, pos, node_size=1000, node_color=color_map, node_shape='8')
 nx.draw_networkx_edges(G, pos, width=2, alpha=0.5)
-nx.draw_networkx_labels(G, pos, font_size=7, font_family="sans-serif", font_weight="bold")
+nx.draw_networkx_labels(G, pos, font_size=8, font_family="sans-serif", font_weight="bold")
 
 # Weights
 edge_labels = nx.get_edge_attributes(G, "weight")
 nx.draw_networkx_edge_labels(G, pos, edge_labels, font_size=6)
+
 
 # Matplotlib visualization
 ax = plt.gca()
 ax.margins(0.08)
 plt.axis("off")
 plt.tight_layout()
+plt.gcf().set_size_inches(15, 8)  # Set the size to 8x5 inches
 plt.show()
+
+# TODO: gameloop
+#while True:
